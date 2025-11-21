4be718073f685f0525ed6fdef24dc7dceb0f2cb4
White-Town <nareshbc5555@gmail.com>
2025-11-21 16:50:05 +0000

    Drag and Drop project started

---
+++ 3-drag-and-drop/script.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll(".card");
+const lists = document.querySelectorAll(".list");
+
+for(const card of cards){
+  card.addEventListener("dragstart", dragStart);
+  card.addEventListener("dragend", dragEnd);
+}
+
+for(const list of lists){
+  list.addEventListener("dragover", dragOver);
+  list.addEventListener("dragenter", dragEnter);
+  list.addEventListener("dragleave", dragLeave);
+  list.addEventListener("drop", dragDrop);
+}
+
+function dragStart(e){
+  e.dataTransfer.setData
+}
