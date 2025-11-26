0f3320d7cec898f93d3bbc24ba1b85c53e8b6bdc
White-Town <nareshbc5555@gmail.com>
2025-11-26 03:56:27 +0000

    Drag and Drop project finished

--- 3-drag-and-drop/script.js
+++ 3-drag-and-drop/script.js
@@ -14,5 +14,30 @@ for(const list of lists){
 }
 
 function dragStart(e){
-  e.dataTransfer.setData
+  //this allows the drop location to know which element is being moved when you release it
+  e.dataTransfer.setData("text/plain",this.id);
+}
+
+function dragEnd(e){
+  console.log("Drag ended");
+}
+function dragOver(e){
+  //this line is important because by default, browser don't allow you to drop elements onto other elements
+  e.preventDefault();
+}
+
+function dragEnter(e){
+  e.preventDefault();
+  this.classList.add("over");
+}
+
+function dragLeave(e){
+  this.classList.remove("over");
+}
+
+function dragDrop(e){
+  const id = e.dataTransfer.getData("text/plain");
+  const card = document.getElementById(id);
+  this.appendChild(card);
+  this.classList.remove("over");
 }
